7b4a6ed10f471b5c89ea37ae2d56c2751fc428c0
Liza <lizadrini6@gmail.com>
2026-01-14 10:32:03 +0100

    first commit

--- src/script.py
+++ src/script.py
@@ -3,16 +3,39 @@ import pandas as pd
 import matplotlib.pyplot
 import seaborn as sns
 
-e_path = #economic data file path
-rt_path = # Real-time data path
-s_path = # Social path
+e_path = ''# Economic data file path
+rt_path = '/fishhealth_norway_rt.csv'# Real-time data path
+s_path = ''# Social path
 
-def load_data(path):
-    return pd.read_csv(path)
+def load_data(path1, path2, path3):
+    social_data = pd.read_csv(path1)
+    economic_data = pd.read_csv(path2)
+    rt_data = pd.read_csv(path3)
+    return social_data, economic_data, rt_data
 
-def clean_data(df):
+def clean_rt_data(df):
+    # Data Cleaning for Social Data
+    if df == social_data:
+        blablabal
+    # Data Cleaning for Economic Data
+    if df == economic_data:
+        blabla
+    # Data Cleaning for Real-Time Data
+    if df == rt_data:
+        for item in df['Status']:
+            if item == 'Påvist':
+                str.replace('Detected')
+            elif item == 'Mistanke':
+                str.replace('Suspected')
+            else:
+                None
+    
+    # Dropping NAs    !!!! DOUBLE CHECK IF NECESSARY!!!!
     if sum(df.isna()) > 100:
         # idkcontinue
+    cleaned_data = df.dropna()
+    return cleaned_data
 
 def visualization(df):
-
+    graphs = [plot for x, y in df] # obv not correct but we'll figure out what exactly we may want to plot
+    return graphs
